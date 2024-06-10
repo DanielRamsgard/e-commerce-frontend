336cb95ea28211589f2a7793961da829ac31bbdb
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-06-09 21:44:25 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className="nav-bar">
+            <NavBar/>
+        </div>
+        <div className="content">
+            <div className="categories">
+                <div className="left">
+                    <div className="box-left-big">
+                        <div className="text anek-malayalam-font">Living Room</div>
+                        <img className="box-left-big-img" src="/static/media/house.png"></img>
+                    </div>
+                    <div className="box-left-small">
+                        <div className="text anek-malayalam-font"> Skincare </div>
+                        <img className="box-left-small-img" src="/static/media/bottle.jpeg"></img>
+                    </div>
+
+                </div>
+                <div className="right">
+                    <div className="box-right">
+                        <div className="text anek-malayalam-font"> Kichen </div>
+                        <img className="box-right-img" src="/static/media/kitchen.jpeg"></img>
+                    </div>
+                    <div className="box-right">
+                        <div className="text anek-malayalam-font"> Electronics </div>
+                        <img className="box-right-img" src="/static/media/electronics.jpeg"></img>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import "./NavBar.css";
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className="nav-container">
+            <div className="logo">
+                <img className="logo-img" src="/static/media/logo"></img>
+            </div>
+            <div className="button-container">
+
+            </div>
+
+        </div>
+    );
+}
+
+export default NavBar;
