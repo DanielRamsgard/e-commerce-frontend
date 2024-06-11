14b32f18e9e79e02dbc83ed45a98adbc8ab15c5b
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-06-11 13:37:56 -0400

    wallpaper

--- src/components/BigCallToAction.js
+++ src/components/BigCallToAction.js
@@ -3,23 +3,25 @@ import React from 'react';
 const BigCallToAction = (props) => {
     if (props.bool){
         return (
-            <>
-                <div className="container-wallpaper">
-                    <div className="wallpaper">
-                        <div className="header">
-                            {props.text}
+            <> 
+                <div className="container-container">
+                    <div className="container-wallpaper">
+                        <div className="wallpaper">
+                            <div className="info-container">
+                                <div className="header">
+                                    {props.text}
+                                </div>
+                                <div className="call-action">
+                                    Our roducts are all made to standard sizes so that you can mix and match them freely.
+                                </div>
+                                <button className="shop-now">
+                                    SHOP NOW
+                                </button>
+                            </div>
                         </div>
-                        <div className="call-action">
-                            Our roducts are all made to standard sizes so that you can mix and match them freely.
+                        <div className="wallpaper-container-img">
+                            <img className="wallpaper-img" src={props.img}></img>
                         </div>
-                        <div className="shop-now-container">
-                            <button className="shop-now">
-                                SHOP NOW
-                            </button>
-                        </div>
-                    </div>
-                    <div>
-                        <img className="wallpaper" src={props.img}></img>
                     </div>
                 </div>
             </>
@@ -27,21 +29,23 @@ const BigCallToAction = (props) => {
     } else {
         return (
             <>
-                <div className="container-wallpaper">
-                    <div>
-                        <img className="wallpaper" src={props.img}></img>
-                    </div>
-                    <div className="wallpaper">
-                        <div className="header-two">
-                            {props.text}
+                <div className="container-container">
+                    <div className="container-wallpaper">
+                        <div className="wallpaper-container-img">
+                            <img className="wallpaper-img" src={props.img}></img>
                         </div>
-                        <div className="call-action">
-                            Our roducts are all made to standard sizes so that you can mix and match them freely.
-                        </div>
-                        <div className="shop-now-container">
-                            <button className="shop-now">
-                                SHOP NOW
-                            </button>
+                        <div className="wallpaper">
+                            <div className="info-container">
+                                <div className="header">
+                                    {props.text}
+                                </div>
+                                <div className="call-action">
+                                    Our roducts are all made to standard sizes so that you can mix and match them freely.
+                                </div>
+                                <button className="shop-now">
+                                    SHOP NOW
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/SmallCallToAction.js
+++ src/components/SmallCallToAction.js
@@ -2,21 +2,25 @@ import React from 'react';
 
 const SmallCallToAction = (props) => {
     return (
-        <div className="container-wallpaper">
-            <div className="wallpaper">
-                <div className="header">
-                    {props.text}
+            <>  
+                <div className="container-container">
+                    <div className="container-wallpaper">
+                        <div className="wallpaper-new">
+                            <div className="info-container">
+                                <div className="header">
+                                    {props.text}
+                                </div>
+                                <div className="call-action">
+                                    Our roducts are all made to standard sizes so that you can mix and match them freely.
+                                </div>
+                                <button className="shop-now">
+                                    SHOP NOW
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="call-action">
-                    Our roducts are all made to standard sizes so that you can mix and match them freely.
-                </div>
-                <div className="shop-now-container">
-                    <button className="shop-now">
-                        SHOP NOW
-                    </button>
-                </div>
-            </div>
-        </div>
+            </>
     );
 }
 
