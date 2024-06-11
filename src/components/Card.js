70d486b9ff74af99a84b6f124e13b127b77f770e
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-06-11 17:33:40 -0400

    cards fixed now

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     return (
         <>
             <div className={props.widthPhone ? "card-new" : "card"}>
-                <div className="card-container-2">
+                <div className={props.widthPhone ? "card-container-2-new"  : "card-container-2" }>
                     <img alt="Card" src={props.img} className="card-img"></img>
                 </div>
                 <div className="card-title">
