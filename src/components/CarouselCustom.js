27fbca7981a24450ce70507523e7e71a9dc5f64c
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-06-11 18:50:57 -0400

    progress

--- src/components/CarouselCustom.js
+++ src/components/CarouselCustom.js
@@ -34,10 +34,10 @@ const CustomCarousel = (props) => {
                         <SmallCard img="/static/media/vase.jpeg" title="Cube Lolo Vase Black" price="$263"/>
                         <SmallCard img="/static/media/shelve.jpeg" title="Insight Series Four Shelf" price="$148"/>
                         <SmallCard img="/static/media/light1.jpeg" title="Here Comes the Sun Pendant" price="$1304"/>
-                        <SmallCard img="/static/media/oil.jpeg" title="Jimmy Boyd Body oil 2000ml" price="$30"/>
-                        <SmallCard img="/static/media/toaster.jpeg" title="GE Stainless Steel Toaster" price="$49"/>
-                        <SmallCard img="/static/media/cabinet2.jpeg" title="Oak Sauder Collection Bookcase" price="$649"/>
-                        <SmallCard img="/static/media/cutting.jpeg" title="Ironwood Charleston Board" price="$117"/>
+                        <SmallCard img="/static/media/spanish2.jpeg" title="Natural Leather Spanish Chair" price="$5040"/>
+                        <SmallCard img="/static/media/tv2.jpeg" title="FHD Flat-Screem Moniter" price="$143"/>
+                        <SmallCard img="/static/media/gold2.jpg" title="Gold Globe Light Fixture" price="$599"/>
+                        <SmallCard img="/static/media/key.jpeg" title="Wireless Mechanical Keyboard" price="$49"/>
                         <SmallCard img="/static/media/whitechair.jpg" title="Traditional Armchair" price="$93"/>
                         <SmallCard img="/static/media/oil.jpeg" title="Jimmy Boyd Body oil 2000ml" price="$30"/>
                         <SmallCard img="/static/media/toaster.jpeg" title="GE Stainless Steel Toaster" price="$49"/>
