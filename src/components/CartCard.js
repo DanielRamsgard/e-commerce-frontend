6efb64dca5455ed3a7d18bb9065b8ca4ba9cf523
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-06-14 12:14:38 -0400

    work

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -5,7 +5,7 @@ const CartCard = () => {
         <>
             <div className="cart-card-container">
                 <div className="left-cart-card">
-
+                    <img className="img-cart-card" alt="Card" src="/static/media/chanel.jpeg"></img>
                 </div>
                 <div className="right-cart-card">
                     
